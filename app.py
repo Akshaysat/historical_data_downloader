fcea968f28de82329f28739794b72691206449de
Akshaysat <akshay.satpaise99@gmail.com>
2022-08-11 12:10:32 +0530

    updated app.py

--- app.py
+++ app.py
@@ -144,5 +144,5 @@ if st.button('Submit'):
 html_string2 = '''<p align = "center">❤️ <a href="https://bit.ly/3SAkFu9" target="_blank">  Spread the word </a></p>'''
 st.markdown(html_string2,unsafe_allow_html=True)
 
-html_string1 = '''<p align = "center">☎️ <a href="https://api.whatsapp.com/send?phone=9198484819808" target="_blank">  Contact us </a></p>'''
+html_string1 = '''<p align = "center">☎️ <a href="https://api.whatsapp.com/send?phone=918484819808" target="_blank">  Contact us </a></p>'''
 st.markdown(html_string1,unsafe_allow_html=True)
